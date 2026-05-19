db2c80615ff762e13fa998b39199b175bbb2f143
RamliWane <ramlisilawane865@gmail.com>
2026-05-19 20:27:28 +0700

    new layouting page bikinlaporan and new package motion

--- src/app/client/bikinlaporan/page.jsx
+++ src/app/client/bikinlaporan/page.jsx
@@ -1,7 +1,31 @@
+import NavbarHome from "../../components/client/NavbarHome";
+import SideBar from "../../components/client/SideBar";
+import FilterCategory from "../../components/client/FilterCategory";
+import LaporanCard from "../../components/client/LaporanCard";
+
 export default function BikinLaporan() {
-    return(
-        <div>
-            <h1>bikin laporan</h1>
+    return (
+        <div className="h-screen flex flex-col overflow-hidden text-black">
+            <NavbarHome />
+            <div className="flex flex-1 overflow-hidden">
+                <SideBar className=" flex-0" />
+
+                <div className="overflow-y-auto overflow-x-hidden no-scrollbar animate-slide-up animate-delay-200">
+                    <header className="flex-1 min-w-0 p-0 sm:p-2 md:p-4 lg:p-4">
+                        <div className="flex flex-col min-w-0 gap-3 p-3">
+                            <section>
+                                <h1 className="text-xl font-bold">BUAT LAPORAN BARU</h1>
+                                <p className="text-sm text-gray-500">Laporkan Masalah Di Sekitar Anda Untuk Membantu Lingkungan Menjadi Lebih Baik</p>
+                            </section>
+                                <FilterCategory />
+                        </div>
+                    </header>
+
+                    <main className="flex flex-col m-5">
+                        <LaporanCard />
+                    </main>
+                </div>
+            </div>
         </div>
     )
 }
