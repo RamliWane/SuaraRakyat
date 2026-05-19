f97bddd68fbfed3040e3a009b1228b582aed42f7
RamliWane <ramlisilawane865@gmail.com>
2026-05-19 23:11:27 +0700

    new layouting at sidebaradmin

--- src/app/components/admin/SideBarAdmin.jsx
+++ src/app/components/admin/SideBarAdmin.jsx
@@ -1,8 +1,9 @@
 "use client"
-import {AdminNavigation, AdminNavigationMenu } from "../../../lib/navigationitems";
+import { AdminNavigation, AdminNavigationMenu} from "../../../lib/navigationitems";
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { useState } from "react";
+
 
 export default function Sidebar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,20 +14,20 @@ export default function Sidebar() {
             ${isOpen ? "translate-x-0 w-50" : "-translate-x-full w-0"}
             lg:translate-x-0 lg:w-50`}>
 
-            <div className="h-full bg-white shadow-xl text-gray-500 p-10 min-w-50">
-                <div className="m-2 space-y-6">
-                    <h3 className="text-[12px] font-bold -ml-5">LIBRARY</h3>
+            <div className="h-full bg-[#1C1A18] shadow-xl text-white p-10 min-w-50">
+                <div className="space-x-2 space-y-3">
+                    <h3 className="text-[12px] font-bold -ml-5">NAVIGATION</h3>
                     <ul className="space-x-2">
                         {AdminNavigation.map((item) => (
                             <Link href={item.href} key={item.label}>
-                                <li className={`flex items-center justify-between gap-5 cursor-pointer ${pathname === item.href ? "text-[#DC9B9B]" : "hover:text-[#DC9B9B] text-gray-500"}`}>    
-                                    <div className="flex gap-2">    
+                                <li className={`flex items-center gap-2 cursor-pointer ${pathname === item.href ? "text-[#DC9B9B]" : "hover:text-[#DC9B9B] text-white"}`}>    
+                                    {pathname === item.href && (
+                                        <div className="absolute left-3 w-1 h-6 bg-[#DC9B9B] rounded-full" />
+                                    )}
+                                    <div className="flex gap-2 text-[15px]">    
                                         {item.icon}
                                         {item.label}
                                     </div>
-                                    {pathname === item.href && (
-                                        <div className="w-1.5 h-6 pl-1 bg-[#DC9B9B] rounded-full"/>
-                                    )}
                                 </li>
                             </Link>
                         ))}
@@ -35,7 +36,7 @@ export default function Sidebar() {
                     <ul className="space-x-2">
                         {AdminNavigationMenu.map((item) => (
                             <Link href={item.href} key={item.label}>
-                                <li className={`flex items-center gap-1 cursor-pointer ${pathname === item.href ? "text-[#DC9B9B]" : "hover:text-[#DC9B9B] text-gray-500"}`}>
+                                <li className={`flex items-center gap-1 text-[15px] cursor-pointer ${pathname === item.href ? "text-[#DC9B9B]" : "hover:text-[#DC9B9B] text-white"}`}>
                                     {item.icon}
                                     {item.label}
                                 </li>
